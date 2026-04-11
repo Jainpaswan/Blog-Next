a3994cb3daa2d636df51bb2ac85bb036bbffc2b8
Jain <jainkumar1050@gmail.com>
2026-04-11 21:47:26 +0530

    Signup page, form ui,layotus has created today

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,5 +1,70 @@
+"use client";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { SignUpSchema } from "@/app/schemas/auth";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function SignUpPage(){
-    return<>
-    <h1 className="text-2xl font-bold">Sign Up Here</h1>
-    </>
+
+    const form=useForm({
+        resolver:zodResolver(SignUpSchema),
+        defaultValues:{
+            name:"",
+            email:"",
+            password:""
+        }
+    });
+
+    function onSubmit(){
+        console.log("Form Values", form.getValues());
+    }
+
+    return (
+         <Card>
+        <CardHeader>
+            <CardTitle>Sign Up</CardTitle>
+            <CardDescription>Create your account to get started.</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FieldGroup className="gap-y-5">    
+                    <Controller name="name" control={form.control} render={({ field,fieldState })=>(
+                        <Field>
+                            <FieldLabel>Full Name</FieldLabel>
+                            <Input aria-invalid={fieldState.invalid} placeholder="Jon Doe" {...field} />
+                            {fieldState.invalid && (<FieldError errors={[fieldState.error]}/>)}
+                        </Field>
+                    )}>
+
+                    </Controller>
+
+                    <Controller name="email" control={form.control} render={({ field,fieldState })=>(
+                        <Field>
+                            <FieldLabel>Email</FieldLabel>
+                            <Input aria-invalid={fieldState.invalid} placeholder="jon.doe@example.com" {...field} />
+                            {fieldState.invalid && (<FieldError errors={[fieldState.error]}/>)}
+                        </Field>
+                    )}>
+
+                    </Controller>
+
+                    <Controller name="password" control={form.control} render={({ field,fieldState })=>(
+                        <Field>
+                            <FieldLabel>Password</FieldLabel>
+                            <Input aria-invalid={fieldState.invalid} type="password" placeholder="••••••••" {...field} />
+                            {fieldState.invalid && (<FieldError errors={[fieldState.error]}/>)}
+                        </Field>
+                    )}>
+
+                    </Controller>
+                    <Button>Sign Up</Button>
+
+                </FieldGroup>
+            </form>
+        </CardContent>
+    </Card>
+    );
 }
---
+++ app/schemas/auth.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const SignUpSchema = z.object({
+    name: z.string().min(3, "Name must be at least 3 characters long").max(30, "Name must be at most 30 characters long"),
+    email: z.email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long").max(30, "Password must be at most 30 characters long"),
+});
